9efdaac227c02d944e345a5e58afab08e122bfc3
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 12:12:07 +0100

    first commit

---
+++ src/main/java/com/qa/springtest/ToDoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
