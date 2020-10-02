d23d9ebe5d3acfa454310207700523f025764d51
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 15:11:22 +0100

    update userservice

--- src/main/java/com/qa/todo/service/UserService.java
+++ src/main/java/com/qa/todo/service/UserService.java
@@ -1,5 +1,12 @@
 package com.qa.todo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.todo.persistence.repo.UserRepository;
+
+@Service
 public class UserService {
 
+	private UserRepository repo;
+	private ModelMapper mapper;
 }
