9b9c74b38eb0e1787830a5dce2802c44efaa5c52
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 12:24:57 +0100

    add package structure, tasks and users

--- src/main/java/com/qa/todo/ToDoApplication.java
+++ src/main/java/com/qa/todo/ToDoApplication.java
@@ -1,4 +1,4 @@
-package com.qa.springtest;
+package com.qa.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/qa/todo/persistence/domain/Tasks.java
@@ -0,0 +1,5 @@
+package com.qa.todo.persistence.domain;
+
+public class Tasks {
+
+}
