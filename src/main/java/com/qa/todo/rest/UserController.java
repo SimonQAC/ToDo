88dfbdb20826a6af01b40fdc81b639d340ee775c
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 15:10:18 +0100

    update usercontroller

--- src/main/java/com/qa/todo/rest/UserController.java
+++ src/main/java/com/qa/todo/rest/UserController.java
@@ -1,5 +1,14 @@
 package com.qa.todo.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
 public class UserController {
+	
+	public UserService service;
 
 }
