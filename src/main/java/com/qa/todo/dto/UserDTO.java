89b8865f03dc2d386d8442fa3b125f90558ca0e7
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 15:05:55 +0100

    update userdto

--- src/main/java/com/qa/todo/dto/UserDTO.java
+++ src/main/java/com/qa/todo/dto/UserDTO.java
@@ -1,5 +1,22 @@
 package com.qa.todo.dto;
 
-public class UserDTO {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-}
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
+@ToString
+public class UserDTO{
+	
+		private Long id;
+		private String name;
+		private String timezone;
+		
+	}
