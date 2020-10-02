520847039dbb6aacb17d25faa5386cfd4882ef3c
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-02 15:09:02 +0100

    update userrepository

--- src/main/java/com/qa/todo/persistence/repo/UserRepository.java
+++ src/main/java/com/qa/todo/persistence/repo/UserRepository.java
@@ -1,5 +1,11 @@
 package com.qa.todo.persistence.repo;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.todo.persistence.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
