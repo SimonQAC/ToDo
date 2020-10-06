605d25189c499ba9f41cd834c41394e89efbe6c6
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-06 15:46:45 +0100

    fix user

--- src/main/java/com/qa/todo/persistence/domain/User.java
+++ src/main/java/com/qa/todo/persistence/domain/User.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Size;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
