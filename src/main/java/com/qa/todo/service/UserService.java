1f59c7fb9fe990d236d996dcd8f200dc1f043418
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-06 12:25:04 +0100

    update userservice

--- src/main/java/com/qa/todo/service/UserService.java
+++ src/main/java/com/qa/todo/service/UserService.java
@@ -2,11 +2,17 @@ package com.qa.todo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 
 import com.qa.todo.dto.UserDTO;
+import com.qa.todo.exception.UserNotFoundException;
 import com.qa.todo.persistence.domain.User;
 import com.qa.todo.persistence.repo.UserRepository;
+import com.qa.todo.utils.SpringBeanUtils;
 
 @Service
 public class UserService {
@@ -25,7 +31,37 @@ public class UserService {
 		return this.mapper.map(user,  UserDTO.class);
 	}
 	
+	private User mapFromDTO(UserDTO userDTO) {
+		return this.mapper.map(userDTO, User.class);
+	}
 	
+	public UserDTO create (User user) {
+		User created = this.repo.save(user);
+		UserDTO mapped = this.mapToDTO(created);
+		return mapped;
+	}
+	
+	public List<UserDTO> read(){
+		List<User> found = this.repo.findAll();
+		List<UserDTO> streamed = found.stream().map(this::mapToDTO).collect(Collectors.toList());
+		return streamed;
+	}
+	
+	public UserDTO read(Long id) {
+		User found = this.repo.findById(id).orElseThrow(UserNotFoundException::new);
+		return this.mapToDTO(found);
+	}
+	
+	public UserDTO update(UserDTO userDTO, Long id) {
+		User toUpdate = this.repo.findById(id).orElseThrow(UserNotFoundException::new);
+		SpringBeanUtils.mergeObject(userDTO, toUpdate);
+		return this.mapToDTO(this.repo.save(toUpdate));
+	}
+	
+	public boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return this.repo.existsById(id);
+	}
 	
 }
 
