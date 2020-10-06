13f383683e94b06a5600e49e367108c8648ee8d7
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-06 12:11:14 +0100

    update pom, userservice

--- src/main/java/com/qa/todo/service/UserService.java
+++ src/main/java/com/qa/todo/service/UserService.java
@@ -1,7 +1,11 @@
 package com.qa.todo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
 
+import com.qa.todo.dto.UserDTO;
+import com.qa.todo.persistence.domain.User;
 import com.qa.todo.persistence.repo.UserRepository;
 
 @Service
@@ -9,4 +13,19 @@ public class UserService {
 
 	private UserRepository repo;
 	private ModelMapper mapper;
+
+	@Autowired
+	public UserService (UserRepository repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	private UserDTO mapToDTO (User user) {
+		return this.mapper.map(user,  UserDTO.class);
+	}
+	
+	
+	
 }
+
