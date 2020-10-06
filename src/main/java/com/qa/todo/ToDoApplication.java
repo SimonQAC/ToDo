0c1f15834b6e6b9cfe7170b5477b34b8883b7bb4
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-06 12:51:27 +0100

    update pom and todoapplication

--- src/main/java/com/qa/todo/ToDoApplication.java
+++ src/main/java/com/qa/todo/ToDoApplication.java
@@ -2,7 +2,12 @@ package com.qa.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableWebMvc
 @SpringBootApplication
 public class ToDoApplication {
 
