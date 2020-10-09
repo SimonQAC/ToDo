bb4f2ceed64c98aea688b2d9d3c0567561a1f1a7
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-09 08:22:01 +0100

    update taskcreate to specify between user and task creation, update createtasktest

--- src/test/java/com/qa/todo/selenium/CreateTaskTest.java
+++ src/test/java/com/qa/todo/selenium/CreateTaskTest.java
@@ -1,7 +1,12 @@
 package com.qa.todo.selenium;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,9 +21,9 @@ public class CreateTaskTest {
 	
 	@BeforeClass
 	public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
-		System.setProperty("wevdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//		options.setHeadless(true);
 		driver = new ChromeDriver(options);
 		driver.manage().window().setSize(new Dimension(1280,720));
 	}
@@ -26,7 +31,37 @@ public class CreateTaskTest {
 	@Test
 	public void createTaskPageTest() {
 		driver.get("http://127.0.0.1:5500/html/index.html");
-        driver.findElement(By.xpath("(//*[@id=\"navbarNavAltMarkup\"]/div/a[2]")).click();
+        driver.findElement(By.xpath("/html/body/div/nav/div/div/a[2]")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String title = driver.getTitle();
+        assertEquals("ToDo List - Task", title);
+	}
+	
+	@Test
+	public void createTaskTest() {
+	    driver.get("http://127.0.0.1:5500/html/index.html");
+	    driver.findElement(By.linkText("User")).click();
+	    driver.findElement(By.linkText("Create")).click();
+	    driver.findElement(By.id("name")).click();
+	    driver.findElement(By.id("name")).sendKeys("Pinky");
+	    driver.findElement(By.id("timezone")).click();
+	    driver.findElement(By.id("timezone")).sendKeys("UTC");
+	    driver.findElement(By.cssSelector(".btn")).click();
+	    assertThat(driver.switchTo().alert().getText(), is("User Successfully Created!"));
+	    driver.findElement(By.linkText("Task")).click();
+	    driver.findElement(By.linkText("Create")).click();
+	    driver.findElement(By.id("name")).click();
+	    driver.findElement(By.id("name")).sendKeys("Take over the world");
+	    driver.findElement(By.id("user_id")).click();
+	    driver.findElement(By.id("user_id")).sendKeys("1");
+	    driver.findElement(By.cssSelector(".btn")).click();
+	    assertThat(driver.switchTo().alert().getText(), is("User Successfully Created!"));
+	    driver.findElement(By.linkText("Task")).click();
+	    driver.findElement(By.linkText("User")).click();
+	}
+	
+	@AfterClass
+	public static void shutdown() {
+		driver.close();
 	}
 }
