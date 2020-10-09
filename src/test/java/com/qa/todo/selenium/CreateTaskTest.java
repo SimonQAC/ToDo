5212cb804c3755e8de446c43dd622b27d39223e0
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-09 07:54:10 +0100

    update frontend to include testable page titles

--- src/test/java/com/qa/todo/selenium/CreateTaskTest.java
+++ src/test/java/com/qa/todo/selenium/CreateTaskTest.java
@@ -1,6 +1,10 @@
 package com.qa.todo.selenium;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,11 +15,18 @@ public class CreateTaskTest {
 	public static WebDriver driver;
 	
 	@BeforeClass
-	public static void init() {
+	public static void setup() {
 		ChromeOptions options = new ChromeOptions();
 		options.setHeadless(true);
-		System.setProperty("wevdriver.chrome.driver", "C:\\Users\\Work\\Documents\\workspace-spring-tool-suite-4-4.8.0.RELEASE\\ToDo\\src\\test\\resources\\chromedriver.exe");
+		System.setProperty("wevdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 		driver = new ChromeDriver(options);
 		driver.manage().window().setSize(new Dimension(1280,720));
+	}
+	
+	@Test
+	public void createTaskPageTest() {
+		driver.get("http://127.0.0.1:5500/html/index.html");
+        driver.findElement(By.xpath("(//*[@id=\"navbarNavAltMarkup\"]/div/a[2]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 }
